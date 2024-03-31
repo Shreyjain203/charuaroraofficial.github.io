e6e04e013cb3d86dc28975088ea2ea33333f9b45
noob geek <shreyjain203@gmail.com>
2024-03-30 23:52:40 -0700

    Converted from static to flask webapp

--- main.py
+++ main.py
@@ -4,11 +4,11 @@ import pandas as pd
 app = Flask(__name__)
 
 index = pd.read_excel("static/data/data.xlsx",
-                      sheet_name='index')
+                      sheet_name='index_page')
 
 collection_list = pd.read_excel("static/data/data.xlsx",
                                 sheet_name='collection_list')
-collections = list(collection_list['Content'].values)
+collections_ = list(collection_list['Content'].values)
     
 # need a repository for all the images
 
@@ -28,34 +28,37 @@ def about_us():
 @app.route('/collections')
 def collections():
     return render_template('collections.html',
-                           collections=collections)
+                           collections=collections_)
 
-@app.route('/collections/<collection_type>')
-def collection_type(collection_type):
-    if collection_type not in collections:
+@app.route('/collections/<collection_type_>')
+def collection_type(collection_type_):
+    if collection_type_ not in collections_:
         return "404 not found"
+    
     product_df = pd.read_excel("static/data/data.xlsx",
-                               sheet_name=collection_type)  # [['names','price']]
-
+                               sheet_name=collection_type_)  # [['outfit names','price']]
+    
+    # return str(product_df)
+    # return product_df.values.tolist()
     return render_template('only_one_category_all_data.html',
                            data=product_df.values.tolist())
 
-@app.route('/collections/<collection_type>/<product>')
-def collection_type(collection_type, product):
-    if collection_type not in collections:
+@app.route('/collections/<collection_type_>/<product>')
+def product_type(collection_type_, product):
+    if collection_type_ not in collections_:
         return "404 not found"
 
     product_df = pd.read_excel("static/data/data.xlsx",
-                               sheet_name=collection_type)
-    products = list(product_df['names'].values)
+                               sheet_name=collection_type_)
+    products = list(product_df['outfit names'].values)
 
     if product not in products:
         return "404 not found"
 
-    data_df = product_df[product_df['names']==product].head(1)
-    return render_template('only_one_category_all_data.html',
-                           collection_type=collection_type,
-                           data=data_df.values.tolist())
+    data_df = product_df[product_df['outfit names']==product].head(1)
+    return render_template('about_one_particular_category.html',
+                           collection_type=collection_type_,
+                           data=data_df.values.tolist()[0])
 
 # Celebrity Influencers page
 @app.route('/celebrity_influencers')
@@ -71,33 +74,3 @@ def contact_us():
 if __name__ == '__main__':
     app.run(debug=True)
 
-
-################################ Rough work ################################
-
-# <!DOCTYPE html>
-# <html>
-# <head>
-#     <title>Dynamic Content</title>
-# </head>
-# <body>
-#     <h1>{{ heading }}</h1>
-#     <img src="{{ url_for('static', filename='assets/' + lehengas_type + '/example.jpg') }}" alt="Example Image">
-#     <a href="{{ url_for('content', content_type='about') }}">About</a>
-#     <a href="{{ url_for('content', content_type='contact') }}">Contact</a>
-# </body>
-# </html>
-
-
-# {% for item in items %}
-# <article>
-#     <header>
-#         <!-- <span class="date">April 22, 2017</span> -->
-#         <h2><a href="#">{{ item.title }}</a></h2>
-#     </header>
-#     <a href="#" class="image fit"><img src="{{ url_for('static', filename='images/' + item.image) }}" alt="" /></a>
-#     <!-- <p>{{ item.description }}</p> -->
-#     <ul class="actions special">
-#         <li><a href="{{ item.whatsapp_link }}" class="button">shop on whatsapp</a></li>
-#     </ul>
-# </article>
-# {% endfor %}
